60962880a8b2f5b5bb5fb709168e97be1faa951d
Navneet Singh <navneetsingh@Navneets-MacBook-Air.local>
2026-04-28 21:04:16 +0530

    backend api fix

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,6 +1,6 @@
 const API_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:3000/api"
-    : "https://internship-placement-tracker.vercel.app/api";
+    : "https://internship-placement-tracker-backen.vercel.app/api";
 
 export default API_URL;
